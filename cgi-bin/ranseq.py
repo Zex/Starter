21759b00af3ea174c17457f59187c4128fbeb524
zex <top_zlynch@yahoo.com>
2014-10-31 21:29:03 +0800

    add navigator to ranseq

--- cgi-bin/ranseq.py
+++ cgi-bin/ranseq.py
@@ -5,71 +5,73 @@
 
 from random import randint#, choice
 from string import digits
+from navi import *
 
 def ranseq(length):
-#    print "--------------------------------------------------------->"
+#    ret += "--------------------------------------------------------->"
 #    ancient_a = [int(choice(digits)) for i in xrange(int(length))]
 #    ancient_b = [int(choice(digits)) for i in xrange(int(length))]
     ancient_a = [randint(1, 1000) for i in xrange(int(length))]
     ancient_b = [randint(1, 1000) for i in xrange(int(length))]
 
-    print "<tr>"
-    print "<td>Ancient a: </td><td><span contenteditable=\"false\">" + str(ancient_a) + "</span></td>"
-    print "</tr>"
-    print "<tr>"
-    print "<td>Ancient b: </td><td><span contenteditable=\"false\">" + str(ancient_b) + "</span></td>"
-    print "</tr>"
+    ret = ""
+    ret += "<tr>"
+    ret += "<td>Ancient a: </td><td><span contenteditable=\"false\">" + str(ancient_a) + "</span></td>"
+    ret += "</tr>"
+    ret += "<tr>"
+    ret += "<td>Ancient b: </td><td><span contenteditable=\"false\">" + str(ancient_b) + "</span></td>"
+    ret += "</tr>"
 
     ancient_a.extend(ancient_b)
     ancient_a.sort()
 
-    print "<tr>"
-    print "<td>Ancient a: </td><td><span contenteditable=\"false\">" + str(ancient_a) + "</span></td>"
-    print "</tr>"
+    ret += "<tr>"
+    ret += "<td>Ancient a: </td><td><span contenteditable=\"false\">" + str(ancient_a) + "</span></td>"
+    ret += "</tr>"
 
     child_min = ancient_a[:(len(ancient_a)-2)/2]
     child_max = ancient_a[(len(ancient_a)-2)/2:-2]
 
-    print "<tr>"
-    print "<td>child_min: </td><td><span contenteditable=\"false\">" + str(child_min) + "</span></td>"
-    print "</tr>"
-    print "<tr>"
-    print "<td>child_max: </td><td><span contenteditable=\"false\">" + str(child_max) + "</span></td>"
-    print "</tr>"
+    ret += "<tr>"
+    ret += "<td>child_min: </td><td><span contenteditable=\"false\">" + str(child_min) + "</span></td>"
+    ret += "</tr>"
+    ret += "<tr>"
+    ret += "<td>child_max: </td><td><span contenteditable=\"false\">" + str(child_max) + "</span></td>"
+    ret += "</tr>"
 
     child_min.append(ancient_a[-1])
     child_max.append(ancient_a[-2])
 
-    print "<tr>"
-    print "<td>child_min sum: </td><td><span contenteditable=\"false\">" + str(reduce(lambda x,y:x+y, child_min)) + "</span></td>"
-    print "</tr>"
-    print "<tr>"
-    print "<td>child_max sum: </td><td><span contenteditable=\"false\">" + str(reduce(lambda x,y:x+y, child_max)) + "</span></td>"
-    print "</tr>"
-#    print "---------------------------------------------------------+"
-    print "</table>"
+    ret += "<tr>"
+    ret += "<td>child_min sum: </td><td><span contenteditable=\"false\">" + str(reduce(lambda x,y:x+y, child_min)) + "</span></td>"
+    ret += "</tr>"
+    ret += "<tr>"
+    ret += "<td>child_max sum: </td><td><span contenteditable=\"false\">" + str(reduce(lambda x,y:x+y, child_max)) + "</span></td>"
+    ret += "</tr>"
+#    ret += "---------------------------------------------------------+"
+    ret += "</table>"
+    return ret
 
 def reply():
     import cgi
     title = "Random Seq"
+   
+    ret  = ""
+    ret += "Content-Type: text/html\n\n"
+    ret += "<!DOCTYPE html>"
+    ret += "<html>"
     
-    print "Content-Type: text/html\n\n"
-    print "<!DOCTYPE html>"
-    print "<html>"
-    
-    print "<head>"
-    print "<title>", title, "</title>"
-    print "<link href=\"/css/basic.css\" rel=\"stylesheet\" type=\"text/css\">"
-    print "<meta charset=\"UTF-8\">"
-    print "</head>"
-    
-    print "<body>"
+    ret += default_head(title) 
+    ret += default_navigator() 
+
+    ret += "<body>"
     
     for i in [100, 10, 1000, 999, 3]:
-        print "<h2>" + "Ranseq with " + str(i) + "</h2>"
-        print ranseq(i)
+        ret += "<h2>" + "Ranseq with " + str(i) + "</h2>"
+        ret += ranseq(i)
     
-    print "</body>"
-    print "</html>"
+    ret += "</body>"
+    ret += "</html>"
 
+    print ret
 reply()
