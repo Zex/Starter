1e43ccdf5496c632eaac83f5de8c2b5ca1cc8db6
Zex <top_zlynch@yahoo.com>
2014-11-04 05:53:50 -0800

    add script/add_permition.sh add script/del_permition.sh

---
+++ script/del_permition.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+RE_TEXT='^.*\(\(default\||README\)\|\(\.\(png\|bmp\|jpg\|php\|html\|conf\|Sample\|xls\|doc\|pdf\|css\)\)\)$'
+
+find . -type f -regex $RE_TEXT -exec echo {} \; -exec chmod a-x {} \;
