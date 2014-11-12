225b56628e9e3dd9aa34ead2b3e4061885fc2a0e
Zex <top_zlynch@yahoo.com>
2014-11-12 09:25:14 -0800

    update script

--- script/del_permition.sh
+++ script/del_permition.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-RE_TEXT='^.*\(\(default\|README\)\|\(\.\(png\|bmp\|jpg\|php\|html\|conf\|Sample\|xls\|doc\|pdf\|css\)\)\)$'
+RE_TEXT='^.*\(\(default\|README\)\|\(\.\(png\|bmp\|jpg\|php\|html\|conf\|Sample\|xls\|doc\|pdf\|css\|sql\)\)\)$'
 
 find . -type f -regex $RE_TEXT -exec echo {} \; -exec chmod a-x {} \;
+
+chown www-data.www-data dbs/ -R
