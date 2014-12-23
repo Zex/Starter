0a58337f019c9dfb0c47bc570182a6ea6a8fe118
zex <top_zlynch@yahoo.com>
2014-12-23 22:10:59 +0800

    add script/json/basic.py

---
+++ script/json/basic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#
+# Author: Zex <top_zlynch@yahoo.com>
+#
+
+RESPONSE_DIR = 'response'
+USERNAME = 'snooze'
+PASSWORD = 'sneezy'
+HOST = '127.0.0.1'
+PORT = '7312'
+URL = 'http://' + HOST + ':' + PORT
