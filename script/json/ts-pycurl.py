3872fa852a3908466bdab1762b1160ce6e165ae0
zex <top_zlynch@yahoo.com>
2014-12-25 23:00:48 +0800

    change response file

--- script/json/ts-pycurl.py
+++ script/json/ts-pycurl.py
@@ -25,16 +25,31 @@ def case():
 
     conn = pycurl.Curl()
 
-    url = 'https://api.github.com/users/Zex/repos'
+    #url = 'https://api.github.com/users/Zex/repos'
+    url = URL
     connbuf = StringIO()
 
     conn.setopt(conn.URL, url)
+    conn.setopt(conn.HEADERFUNCTION, connbuf.write)
     conn.setopt(conn.WRITEFUNCTION, connbuf.write)
     conn.setopt(conn.HTTPHEADER, [str(x)+':'+str(headers[x]) for x in headers.keys()])
     conn.perform()
+  
+    with open(RESPONSE_DIR+'/rsp_'+url.replace('/','-').replace(':', '_')+'.json', 'w') as fd:
+        fd.write('#' + url + '\n' + connbuf.getvalue() + '\n')
 
-    with open(RESPONSE_DIR+'/rsp_'+url.replace('/','.')+'.json', 'w') as fd:
-        fd.write('#headers:\n' + connbuf.getvalue() + '\n')
+    connbuf.close()
+
+    connbuf = StringIO()
+
+    url = URL + '/accesspoint'
+    conn.setopt(conn.URL, url)
+    conn.setopt(conn.HEADERFUNCTION, connbuf.write)
+    conn.setopt(conn.WRITEFUNCTION, connbuf.write)
+    conn.setopt(conn.HTTPHEADER, [str(x)+':'+str(headers[x]) for x in headers.keys()])
+    conn.perform()
+    with open(RESPONSE_DIR+'/rsp_'+url.replace('/','-').replace(':', '_')+'.json', 'w') as fd:
+        fd.write('#' + url + '\n' + connbuf.getvalue() + '\n')
 
     connbuf.close()
 
