2c1cece057530486d0d7533064fcfd54335e5e88
zex <top_zlynch@yahoo.com>
2014-12-25 23:00:36 +0800

    change response file

--- script/json/ts-httplib2.py
+++ script/json/ts-httplib2.py
@@ -16,27 +16,36 @@ def case():
 
     headers = {
         #'Content-Type' : 'application/json'
-        #'Content-Type' : 'text/html',
+        'Content-Type' : 'text/plain',
         }
 
     data = {
+            'username' : USERNAME,
+            'password' : PASSWORD
             }
 
-    conn = httplib2.Http()
+    conn = httplib2.Http(".cache")
+    """
+    url = 'http://localhost/sos/login.html'
+    rsp = conn.request(url, 'PUT', body=json.dumps(data), headers=headers)
 
-    url = URL# + '/accesspoint'
+    headers['Cookie'] = rsp[0]['set-cookie']
+    """
+    #conn.add_credentials(USERNAME, PASSWORD)
+    url = URL
     rsp = conn.request(url, 'GET', body=json.dumps(data), headers=headers)
+    #headers['Cookie'] = rsp[0]['set-cookie']
 
-    with open(RESPONSE_DIR+'/rsp_'+url.replace('/','.')+'.json', 'w') as fd:
-        fd.write('#headers:\n' + str(rsp[0]) + '\n')
-        fd.write('#content:\n' + str(rsp[1]) + '\n')
+    with open(RESPONSE_DIR+'/rsp_'+url.replace('/','-').replace(':', '_') +'.json', 'w') as fd:
+        fd.write('#' + url + '\n' + str(rsp[0]) + '\n')
+        fd.write('#' + url + '\n' + str(rsp[1]) + '\n')
 
     url = URL + '/accesspoint'
     rsp = conn.request(url, 'GET', body=json.dumps(data), headers=headers)
     
-    with open(RESPONSE_DIR+'/rsp_'+url.replace('/','.')+'.json', 'w') as fd:
-        fd.write('#headers:\n' + str(rsp[0]) + '\n')
-        fd.write('#content:\n' + str(rsp[1]) + '\n')
+    with open(RESPONSE_DIR+'/rsp_'+url.replace('/','-').replace(':', '_') +'.json', 'w') as fd:
+        fd.write('#' + url + '\n' + str(rsp[0]) + '\n')
+        fd.write('#' + url + '\n' + str(rsp[1]) + '\n')
 
 try:
     case()
